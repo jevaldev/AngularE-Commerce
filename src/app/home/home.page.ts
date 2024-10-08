47a86976523860e3f70985ecf1e16b9cc0aca8b1
ValDEViram <jesusvalvir0354@gmail.com>
2024-10-08 14:16:24 -0600

    Comienzo del proyecto, llamada a la API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  dishes: any[] = [];
+  readonly API: string =
+    'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+
+  getIngredients = (meal: { [key: string]: any }): string[] => {
+    return Object.keys(meal)
+      .filter((key) => key.startsWith('strIngredient') && meal[key]) // Only truthy values
+      .map((key) => meal[key]); // Map to ingredient values
+  };
+
+  async getFoodAPI() {
+    try {
+      const res = await fetch(this.API);
+      const data = await res.json();
+
+      if (data.meals) {
+        const mealData = data.meals.map((meal: { [key: string]: any }) => {
+          const {
+            strMeal = null,
+            strCategory = null,
+            strArea = null,
+            strMealThumb = null,
+            strYoutube = null,
+            strSource = null,
+          } = meal;
+
+          return {
+            strMeal,
+            strCategory,
+            strArea,
+            strMealThumb,
+            strYoutube,
+            strSource,
+            ingredients: this.getIngredients(meal),
+          };
+        });
+
+        this.dishes = mealData;
+        console.log(this.dishes);
+      } else {
+        console.error('No se encontró ningun platillo');
+      }
+    } catch (error) {
+      console.error('Error al capturar los datos de la API', error);
+    }
+  }
 
   constructor() {}
 
+  ngOnInit() {
+    this.getFoodAPI();
+  }
 }
